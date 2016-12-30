584ffb45bea440c16f11314b2aaeeb80ad85c3f5
Goran Ehrsson <goran@technipelago.se>
2016-12-30 16:20:16 +0100

    Upgraded project to Grails 2.4.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,9 +43,9 @@ grails.project.dependency.resolution = {
         compile ":user-tag:1.0"
         compile ":selection:0.9.8"
 
-        compile ":crm-invoice:2.4.1"
-        compile ":crm-security:2.4.2"
-        compile ":crm-ui-bootstrap:2.4.1"
+        compile ":crm-invoice:2.4.2-SNAPSHOT"
+        compile ":crm-security:2.4.3"
+        compile ":crm-ui-bootstrap:2.4.3"
     }
 }
 
